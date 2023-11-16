e02fe89f9c80bc8eb6720357764b3d4fb557c040
Jose Mendez <josemmendezt@gmail.com>
2023-11-15 21:09:31 -0700

    invites UI

--- src/components/home/EmployeesTable.tsx
+++ src/components/home/EmployeesTable.tsx
@@ -10,83 +10,97 @@ import {
 
 import { employees } from '@/tempData/employees'
 import { getPerformance } from '@/components/utils/getPerformance'
+import { Separator } from '../ui'
 
 export default function EmployeesTable() {
   return (
-    <Table>
-      <TableCaption>
-        Performance metrics will appear in the future.
-      </TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Employee</TableHead>
-          <TableHead>Team</TableHead>
-          <TableHead>Position</TableHead>
-          <TableHead>Performance</TableHead>
-          <TableHead>Contributions</TableHead>
-          <TableHead>Budget Left</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {employees.map((employee) => (
-          <TableRow className="cursor-pointer" key={employee.id}>
-            <TableCell className="px-0">
-              <a
-                href={`/employee/${employee.id}`}
-                className="flex items-center  py-4 font-medium"
+    <>
+      <div className="-mt-8 ml-auto w-40 border  px-4 py-1 text-center">
+        {employees.length} / 5 Employees
+      </div>
+      <section className="mt-8">
+        <Table>
+          <TableCaption>
+            <hr className="-mt-4 mb-8" />
+            Performance metrics will appear in the future.
+          </TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Employee</TableHead>
+              <TableHead>Team</TableHead>
+              <TableHead>Position</TableHead>
+              <TableHead>Performance</TableHead>
+              <TableHead>Contributions</TableHead>
+              <TableHead>Budget Left</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {employees.map((employee) => (
+              <TableRow
+                className="cursor-pointer transition duration-300 ease-in-out hover:bg-secondary"
+                key={employee.id}
               >
-                <img
-                  width={60}
-                  className="rounded-full"
-                  src={employee.picture}
-                  alt={employee.name}
-                />
-                <div className="ml-4">{`${employee.name} ${employee.last_name}`}</div>
-              </a>
-            </TableCell>
-            <TableCell className="px-0">
-              <a
-                href={`/employee/${employee.id}`}
-                className="flex h-24 items-center  py-4"
-              >
-                {employee.team}
-              </a>
-            </TableCell>
-            <TableCell className="px-0">
-              <a
-                href={`/employee/${employee.id}`}
-                className="flex h-24 items-center  py-4 "
-              >
-                {employee.position}
-              </a>
-            </TableCell>
-            <TableCell className={getPerformance(employee.performance).class}>
-              <a
-                href={`/employee/${employee.id}`}
-                className="flex  h-24 items-center py-4 "
-              >
-                {getPerformance(employee.performance).text}
-              </a>
-            </TableCell>
-            <TableCell className="px-0">
-              <a
-                href={`/employee/${employee.id}`}
-                className="flex h-24  items-center py-4 "
-              >
-                {employee.contributionHistory}
-              </a>
-            </TableCell>
-            <TableCell className="px-0">
-              <a
-                href={`/employee/${employee.id}`}
-                className="flex h-24  items-center py-4 "
-              >
-                {employee.budgetLeft}
-              </a>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+                <TableCell className="px-0">
+                  <a
+                    href={`/employee/${employee.id}`}
+                    className="flex items-center  py-4 font-medium"
+                  >
+                    <img
+                      width={60}
+                      className="rounded-full"
+                      src={employee.picture}
+                      alt={employee.name}
+                    />
+                    <div className="ml-4">{`${employee.name} ${employee.last_name}`}</div>
+                  </a>
+                </TableCell>
+                <TableCell className="px-0">
+                  <a
+                    href={`/employee/${employee.id}`}
+                    className="flex h-24 items-center  py-4"
+                  >
+                    {employee.team}
+                  </a>
+                </TableCell>
+                <TableCell className="px-0">
+                  <a
+                    href={`/employee/${employee.id}`}
+                    className="flex h-24 items-center  py-4 "
+                  >
+                    {employee.position}
+                  </a>
+                </TableCell>
+                <TableCell
+                  className={getPerformance(employee.performance).class}
+                >
+                  <a
+                    href={`/employee/${employee.id}`}
+                    className="flex  h-24 items-center py-4 "
+                  >
+                    {getPerformance(employee.performance).text}
+                  </a>
+                </TableCell>
+                <TableCell className="px-0">
+                  <a
+                    href={`/employee/${employee.id}`}
+                    className="flex h-24  items-center py-4 "
+                  >
+                    {employee.contributionHistory}
+                  </a>
+                </TableCell>
+                <TableCell className="px-0">
+                  <a
+                    href={`/employee/${employee.id}`}
+                    className="flex h-24  items-center py-4 "
+                  >
+                    {employee.budgetLeft}
+                  </a>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </section>
+    </>
   )
 }
---
+++ src/components/home/inviteEmployees.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import {
+  Button,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Input,
+  Label,
+  Separator,
+} from '../ui'
+
+function InviteEmployees() {
+  const emptyEmployee = {
+    firstName: '',
+    lastName: '',
+    email: '',
+  }
+
+  const [invites, setInvites] = useState([emptyEmployee])
+
+  const newEmployeeCard = () => {
+    setInvites((prev) => [...prev, emptyEmployee])
+  }
+
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Invite Employees</Button>
+        </DialogTrigger>
+        <DialogContent className="min-h-50vh mx-auto max-h-[80vh] w-full overflow-auto">
+          <DialogHeader>
+            <DialogTitle>Invite Employees</DialogTitle>
+            <DialogDescription>
+              Send invites for your employees
+            </DialogDescription>
+          </DialogHeader>
+          {invites.map((invite, index) => (
+            <Card key={index} className=" pt-8">
+              <CardContent>
+                <div className="flex justify-between">
+                  <div className="grid max-h-full w-[48%]  max-w-sm items-center ">
+                    <Label htmlFor="firstName">First Name</Label>
+                    <Input
+                      type="text"
+                      id="firstName"
+                      placeholder="First Name"
+                    />
+                  </div>
+                  <div className="grid max-h-full w-[48%]  max-w-sm items-center ">
+                    <Label htmlFor="lastName">Last Name</Label>
+                    <Input type="text" id="lastName" placeholder="Last Name" />
+                  </div>
+                </div>
+                <div className="mt-2 items-center ">
+                  <Label htmlFor="email">Email</Label>
+                  <Input type="email" id="email" placeholder="email" />
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+
+          <div
+            onClick={newEmployeeCard}
+            className="flex h-12 w-full items-center justify-center rounded-lg border-2 border-dotted bg-secondary  text-center font-semibold text-muted-foreground hover:cursor-pointer"
+          >
+            Add another invite{' '}
+            <span className="foont-bold realtive ml-2 text-3xl">+</span>
+          </div>
+          <DialogFooter className="mt-4">
+            <div>
+              <a href="/">
+                <Button className="mx-auto w-full max-w-sm" type="submit">
+                  Send Invites
+                </Button>
+              </a>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
+
+export default InviteEmployees
