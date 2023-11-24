e14c417be7177bcc360f42c40b94253f1b8f7933
Jose Mendez <josemmendezt@gmail.com>
2023-11-23 17:49:48 -0700

    change folder structure

--- astro.config.js
+++ astro.config.js
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    '/login': '/',
+    '/': '/login',
   },
   integrations: [tailwind(), react()],
   output: 'hybrid',
